c396971651f0299633487d0fc0de91721bd0ce04
Daniel <danielantonio23599@gmail.com>
2023-05-09 11:53:53 -0300

    Update saltosCavalo.pl
    remoção do predicado desnecessário  size

--- saltosCavalo.pl
+++ saltosCavalo.pl
@@ -1,19 +1,12 @@
 % Definindo as posições de movimento do cavalo
 movimentos_cavalo([[-2,-1],[-2,1],[-1,-2],[-1,2],[1,-2],[1,2],[2,-1],[2,1]]).
 
-size(TableOut, Size) :-
-    length(TableOut, SizeRows), % obtém o tamanho das linhas
-    nth0(0, TableOut, Row), % obtém uma das linhas da matriz
-    length(Row, SizeCols),
-    Size is SizeRows * SizeCols. % obtém o tamanho das colunas
-
 % Cria uma tabela de dimensões Size x Size, inicialmente preenchida com unbound variables
 create_table(Size, Table) :-
     length(Row, Size),              % cria uma lista Row de tamanho Size
     maplist(=(0), Row),          % preenche a lista Row com 0s
     length(Table, Size),           % cria uma lista de Size elementos (linhas)
     maplist(=(Row), Table).
-
 
 % Define um valor para uma célula da tabela
 set_cell(X, Y, Value, Table, NewTable) :-
@@ -55,18 +48,18 @@ dentro_da_matriz(X, Y, Matriz) :-
     L is NumLinhas-1,
     between(0, L, Y).
 
-dfs_pruning(X, Y, Table, TableOut, Count) :-
-    size(Table, Size),
+dfs_pruning(N, X, Y, Table, TableOut, Count) :-
+    Size is N*N,
     Size =:= Count,
     set_cell(X, Y, Count, Table, NewTable),
     TableOut = NewTable.
 
-dfs_pruning(X, Y, Table, TableOut, Count) :-
+dfs_pruning(N, X, Y, Table, TableOut, Count) :-
     % Adiciona a posição visitada a matriz com o valor count
     set_cell(X, Y, Count, Table, NewTable),
     nova_posicao_cavalo(X, Y, NewTable, X1, Y1),
     Count1 is Count + 1,
-    dfs_pruning(X1, Y1, NewTable, TableOut, Count1).
+    dfs_pruning(N, X1, Y1, NewTable, TableOut, Count1).
 
 imprimeTab(Tab) :-
    imprimeLinha(Tab).
@@ -86,7 +79,7 @@ saltosCavalo(N, X, Y) :-
    create_table(N, Table),
    Y1 is Y - 1,
    X1 is X - 1,
-   dfs_pruning(X1, Y1, Table, TableOut, 1),
+   dfs_pruning(N, X1, Y1, Table, TableOut, 1),
    % Imprime a sequência de casas visitadas
    write('Matriz de casas visitadas: '), nl,
    imprimeTab(TableOut), nl,
