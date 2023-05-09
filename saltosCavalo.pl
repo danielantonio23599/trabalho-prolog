c6d127eed818cb3be15fd2e7066b943ec1d99f3e
Daniel <danielantonio23599@gmail.com>
2023-05-09 11:45:56 -0300

    Update saltosCavalo.pl
    adição de contagem de processamento

--- saltosCavalo.pl
+++ saltosCavalo.pl
@@ -1,13 +1,13 @@
-% Definindo as posições de movimento do cavalo
+% Definindo as posiÃ§Ãµes de movimento do cavalo
 movimentos_cavalo([[-2,-1],[-2,1],[-1,-2],[-1,2],[1,-2],[1,2],[2,-1],[2,1]]).
 
 size(TableOut, Size) :-
-    length(TableOut, SizeRows), % obtém o tamanho das linhas
-    nth0(0, TableOut, Row), % obtém uma das linhas da matriz
+    length(TableOut, SizeRows), % obtÃ©m o tamanho das linhas
+    nth0(0, TableOut, Row), % obtÃ©m uma das linhas da matriz
     length(Row, SizeCols),
-    Size is SizeRows * SizeCols. % obtém o tamanho das colunas
+    Size is SizeRows * SizeCols. % obtÃ©m o tamanho das colunas
 
-% Cria uma tabela de dimensões Size x Size, inicialmente preenchida com unbound variables
+% Cria uma tabela de dimensÃµes Size x Size, inicialmente preenchida com unbound variables
 create_table(Size, Table) :-
     length(Row, Size),              % cria uma lista Row de tamanho Size
     maplist(=(0), Row),          % preenche a lista Row com 0s
@@ -15,7 +15,7 @@ create_table(Size, Table) :-
     maplist(=(Row), Table).
 
 
-% Define um valor para uma célula da tabela
+% Define um valor para uma cÃ©lula da tabela
 set_cell(X, Y, Value, Table, NewTable) :-
     nth0(Y, Table, Row),
     nth0(X, Row, OldValue),
@@ -27,7 +27,7 @@ replace_at(Index, List, Value, NewList) :-
     nth0(Index, List, _, Rest),
     nth0(Index, NewList, Value, Rest).
 
-% Obtém o valor de uma célula da tabela
+% ObtÃ©m o valor de uma cÃ©lula da tabela
 get_cell(X, Y, Table, Value) :-
     nth0(Y, Table, Row), % retona a linha
     nth0(X, Row, Value). % retorna o valor
@@ -62,7 +62,7 @@ dfs_pruning(X, Y, Table, TableOut, Count) :-
     TableOut = NewTable.
 
 dfs_pruning(X, Y, Table, TableOut, Count) :-
-    % Adiciona a posição visitada a matriz com o valor count
+    % Adiciona a posiÃ§Ã£o visitada a matriz com o valor count
     set_cell(X, Y, Count, Table, NewTable),
     nova_posicao_cavalo(X, Y, NewTable, X1, Y1),
     Count1 is Count + 1,
@@ -81,10 +81,15 @@ imprimeLinha([L|R]) :-
    imprimeLinha(R).
 
 saltosCavalo(N, X, Y) :-
-   % Cria o tabuleiro e a lista de posições visitadas
+   % Cria o tabuleiro .
+   get_time(Inicio),
    create_table(N, Table),
    Y1 is Y - 1,
    X1 is X - 1,
    dfs_pruning(X1, Y1, Table, TableOut, 1),
-    % Imprime a sequência de casas visitadas
-    write('Matriz de casas visitadas: '), nl, imprimeTab(TableOut).
+   % Imprime a sequÃªncia de casas visitadas
+   write('Matriz de casas visitadas: '), nl,
+   imprimeTab(TableOut), nl,
+   get_time(Fim),
+   TemPro is Fim - Inicio,
+   write('Fim processameno: '), write(TemPro), write(' (s)').
